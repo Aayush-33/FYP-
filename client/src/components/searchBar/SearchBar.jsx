5aa55055a8999470f6402dce41cdccbd715cc73a
unknown <np03cs4a220460@heraldcollege.edu.np>
2025-04-07 13:14:35 +0545

    searchBar

---
+++ client/src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import "./searchBar.scss";
+import { Link } from "react-router-dom";
+
+const types = ["buy", "rent"];
+
+function SearchBar() {
+  const [query, setQuery] = useState({
+    type: "buy",
+    city: "",
+    minPrice: "",
+    maxPrice: "",
+  });
+
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
+
+  const handleChange = (e) => {
+    setQuery((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  // Build the search URL with only valid parameters
+  const buildSearchUrl = () => {
+    const params = new URLSearchParams();
+    
+    // Always include type
+    params.append("type", query.type);
+    
+    // Only include other params if they have values
+    if (query.city) params.append("city", query.city);
+    if (query.minPrice) params.append("minPrice", query.minPrice);
+    if (query.maxPrice) params.append("maxPrice", query.maxPrice);
+    
+    return `/list?${params.toString()}`;
+  };
+
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          name="city"
+          placeholder="City"
+          value={query.city}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Min Price"
+          value={query.minPrice}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={10000000}
+          placeholder="Max Price"
+          value={query.maxPrice}
+          onChange={handleChange}
+        />
+        <Link to={buildSearchUrl()}>
+          <button>
+            <img src="/search.png" alt="" />
+          </button>
+        </Link>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
